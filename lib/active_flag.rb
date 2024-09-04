dd2ba0fa8e0f731a102d7daf00c647bb39f00d3c
Kenn Ejima <kenn@users.noreply.github.com>
2024-09-04 17:09:22 +0900

    Support JOIN | Remove op: :and option | v2.0.0

--- lib/active_flag.rb
+++ lib/active_flag.rb
@@ -35,34 +35,28 @@ module ActiveFlag
 
       # Scopes
       define_singleton_method "where_#{column}" do |*args|
-        options, integer, column_name = send "_where_#{column}", *args
-        if options[:op] == :and
-          ActiveSupport::Deprecation.warn('op: :and is deprecated, use where_all instead')
-          where("#{column_name} & #{integer} = #{integer}")
-        else
-          where("#{column_name} & #{integer} > 0")
-        end
+        integer, column_name = send("_where_#{column}", *args)
+        where("#{column_name} & #{integer} > 0")
       end
 
       define_singleton_method "where_all_#{column}" do |*args|
-        _options, integer, column_name = send "_where_#{column}", *args
+        integer, column_name = send("_where_#{column}", *args)
         where("#{column_name} & #{integer} = #{integer}")
       end
 
       define_singleton_method "where_not_#{column}" do |*args|
-        _options, integer, column_name = send "_where_#{column}", *args
+        integer, column_name = send("_where_#{column}", *args)
         where("#{column_name} & #{integer} = 0")
       end
 
       define_singleton_method "where_not_all_#{column}" do |*args|
-        _options, integer, column_name = send "_where_#{column}", *args
+        integer, column_name = send("_where_#{column}", *args)
         where("#{column_name} & #{integer} < #{integer}")
       end
 
       # utility method to extract parameters
       define_singleton_method "_where_#{column}" do |*args|
         return [
-          args.extract_options!,
           active_flags[column].to_i(args),
           "#{connection.quote_table_name(table_name)}.#{connection.quote_column_name(column)}"
         ]
--- lib/active_flag/version.rb
+++ lib/active_flag/version.rb
@@ -1,3 +1,3 @@
 module ActiveFlag
-  VERSION = '1.7.0'
+  VERSION = '2.0.0'
 end
